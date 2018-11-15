7d6b7123173f4759563307b9d68824209fc1785d
Andrew Quaye <andyquayegh@outlook.com>
2018-11-15 09:10:24 +0100

    change db.connection.php
    change db.connection.php

--- controls/db.connection.php
+++ controls/db.connection.php
@@ -6,21 +6,32 @@
  * Time: 7:44 PM
  */
 
-
-if (file_exists("db.server.php")){
-    include_once "db.server.php";
-
-}else{
+if ($_SERVER['HTTP_HOST'] === "localhost"){
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database = "school_data";
-}
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+}else{
+
+    $servername = "localhost";
+    $username = "ghanacucom";
+    $password = "ghanacu@50";
+    $database = "ghanacu_portal";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
 }
